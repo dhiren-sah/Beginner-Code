369edb1aa7251c3f9d052e81d68ad72b02840e20
Dhiren Sah <dhirensah111@gmail.com>
2025-04-22 23:38:35 +0530

    Add counging

--- even.c
+++ even.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 int main()
 {
-    int i, n, sum = 0;
+    int i, n, sum = 0, count = 0;
     printf("Enter any Number: ");
     scanf("%d",&n);
     printf("Even Numbers are : \n");
@@ -11,9 +11,11 @@ int main()
         if(i % 2 == 0)
         {
             printf("%d ",i);
+            count++;
             sum += i;
         }
     }
+    printf("\nTotal Number of Even Number are %d",count);
     printf("\nSum of the Even Number are %d",sum);
     return 0;
 }
