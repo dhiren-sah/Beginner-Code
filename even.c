6d948406d4c4decc0e8f11eb0d8d2781ff7bc787
Dhiren Sah <dhirensah111@gmail.com>
2025-04-22 15:16:24 +0530

    Add find even number between 1-n program

---
+++ even.c
@@ -0,0 +1,19 @@
+// Write a program to find even number between 1- n number and also find thier sum
+#include<stdio.h>
+int main()
+{
+    int i, n, sum = 0;
+    printf("Enter any Number: ");
+    scanf("%d",&n);
+    printf("Even Numbers are : \n");
+    for(i = 1; i < n; i++)
+    {
+        if(i % 2 == 0)
+        {
+            printf("%d ",i);
+            sum += i;
+        }
+    }
+    printf("\nSum of the Even Number are %d",sum);
+    return 0;
+}
